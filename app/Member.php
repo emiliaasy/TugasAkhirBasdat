0f77e5512d1c42c25a53dbf14febaf69816cb7f6
emiliaasy <emilia_sy@apps.ipb.ac.id>
2020-06-14 14:13:38 +0700

    soft deletes

--- app/Member.php
+++ app/Member.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Member extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = ['nama', 'email', 'alamat', 'kontak'];
 }
